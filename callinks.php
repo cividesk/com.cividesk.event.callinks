d8852f0bccacd2455c3c80f1b7a2f0a8a18c2d3f
Yashodha Chaku <yashodha@cividesk.com>
2021-08-04 15:58:40 +0000

    QA done, working

--- callinks.php
+++ callinks.php
@@ -3,60 +3,59 @@
 /**
  * Implements hook_civicrm_alterContent().
  */
-function civicrm_addtocal_alterContent(&$content, $context, $tplName, &$object) {
+function callinks_civicrm_alterContent(&$content, $context, $tplName, &$object) {
+
+  // Determine event_id property based on the page displayed 
   switch ($tplName) {
     case 'CRM/Event/Page/EventInfo.tpl':
       $key = '_id';
       break;
-
     case 'CRM/Event/Form/Registration/ThankYou.tpl':
       $key = '_eventId';
       break;
-
     default:
       return;
   }
 
-watchdog('callinks', 'Ben was here');
-
+  // Abort if incorrect or not initialized propertly
   if (empty($object->{$key}) || strpos($content, 'iCal_links-section') === FALSE) {
     return;
   }
 
-
+  // Initialize variables
   $event_id = $object->{$key};
   $links = [];
 
   // iCal link (native CiviCRM)
   $links['ical'] = [
-    'title' => t('iCalendar'),
-    'icon' => 'calendar-plus',
+    'title' => t('Add to desktop calendar'),
+    'icon' => 'calendar-plus-o',
     'path' => url('civicrm/event/ical', [
       'query' => ['reset' => 1, 'id' => $event_id],
     ]),
   ];
 
   // Google Calendar link
-  include_once('includes/civicrm_add_to_calendar.gcalendar');
+  require_once('includes/civicrm_add_to_calendar.gcalendar.inc');
   if ($path = civicrm_add_to_calendar_build_gcalendar_url($event_id)) {
     $links['gcalendar'] = [
-      'title' => t('Google Calendar'),
+      'title' => t('Add to Google calendar'),
       'icon' => 'google-plus-square',
       'path' => $path,
     ];
   }
 
   // Calculate the replacement string
-  $replacement = '';
+  $links_html = '';
   foreach ($links as $link) {
-    $replacement .= "<a href='$link[path]' title='$link[title]'><i class='crm-i fa-$link[icon]'></i></a>";
+    $css = "font-size: 32px; margin-right: 8px;";
+    $links_html .= "<a href='$link[path]' title='$link[title]'><i class='crm-i fa-$link[icon]' style='$css'></i></a>";
   }
 
-watchdog('callinks', $replacement);
-
-  // Perform the replacement in the page
-  $anchor = '<div class="action-link section iCal_links-section">';
-  $closed = '<\\/div>'; // escape \ because of the regexp
-  $content = preg_replace($anchor.'(.*)'.$closed, $anchor.$replacement.$closed, $content);
+  // Perform the replacement in the page - minimal anchor to be more robust against future changes
+  $anchor = 'iCal_links-section">';    // <div class="action-link section iCal_links-section">
+  $closed = '<\\/div>';                // </div>, but / is special char for regexp so escaped
+  $pattern = '/' . $anchor . '.*?' . $closed . '/ms';  // ?: ungreedy, m: multiline, s: . includes newline chars
+  $replacement = $anchor . $links_html . str_replace($closed, '\\', '');
+  $content = preg_replace($pattern, $replacement, $content);
 }
-
